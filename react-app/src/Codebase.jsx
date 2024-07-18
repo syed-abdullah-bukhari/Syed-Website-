049f83a2cbe67e3e162c25215788078228f7b759
Syed Abdullah Bukhari <syedabdullahbukhari77@gmail.com>
2024-07-18 16:55:12 +0500

    deleted the all the old css

--- react-app/src/Codebase.jsx
+++ react-app/src/Codebase.jsx
@@ -5,15 +5,14 @@ export default function CodeBase () {
 
     const[name , SetName] = useState('Khuzaima Khan');
 
-    const UpdateName = () => 
-    {
+    const OnHandleClick = ()  => {
         SetName('Syed Abdullah Bukhari');
     }
 
     return(
         <div className='Container glass'>
             <h3>Name : {name}</h3>
-            <button onClick={UpdateName}>Set Name:  </button>
+            <button className='Glowing-btn' onClick={UpdateName}>Set Name </button>
         </div>
     )
 
